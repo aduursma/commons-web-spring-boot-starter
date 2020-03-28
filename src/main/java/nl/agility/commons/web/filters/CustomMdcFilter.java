916cb1b05b036cd6e1fd3199358598bdb0f8861d
aduursma <andre@agility-it-services.nl>
2020-03-28 12:17:29 +0100

    Updated import statements

--- src/main/java/nl/agility/commons/web/filters/CustomMdcFilter.java
+++ src/main/java/nl/agility/commons/web/filters/CustomMdcFilter.java
@@ -3,7 +3,11 @@ package nl.agility.commons.web.filters;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.UUID;
