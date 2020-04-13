1081f293f0364e90b1d150c564c4ee200c74cca7
aduursma <andre@agility-it-services.nl>
2020-04-13 13:47:19 +0200

    Do not secure API endpoints

--- src/main/java/nl/agility/commons/web/config/SecurityAutoConfiguration.java
+++ src/main/java/nl/agility/commons/web/config/SecurityAutoConfiguration.java
@@ -2,28 +2,37 @@ package nl.agility.commons.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
+@EnableWebSecurity
 public class SecurityAutoConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-            .antMatchers("/actuator/health").permitAll()
-            .antMatchers("/actuator/info").permitAll()
-            .antMatchers("/actuator/**").hasRole("OPERATIONS")
-            .anyRequest().permitAll()
-            .and()
-            .httpBasic()
-            .and()
-            .headers()
-            .frameOptions().sameOrigin()
-            .addHeaderWriter(new StaticHeadersWriter("Server", "Unknown"))
-            .and()
-            .csrf().disable();
+        configureApiUris(http);
+
+//        http
+//            .authorizeRequests()
+//            .antMatchers("/actuator/health").permitAll()
+//            .antMatchers("/actuator/info").permitAll()
+//            .antMatchers("/actuator/**").hasRole("OPERATIONS")
+////            .anyRequest().permitAll()
+//            .antMatchers("/api/**").permitAll()
+//            .and()
+//            .httpBasic()
+//            .and()
+//            .headers()
+//            .frameOptions().sameOrigin()
+//            .addHeaderWriter(new StaticHeadersWriter("Server", "Unknown"))
+//            .and()
+//            .csrf().disable();
+    }
+
+    protected void configureApiUris(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/**").permitAll();
     }
 
 }
